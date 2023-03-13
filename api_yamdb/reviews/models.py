91fd5e7bec719a4099a1a2e6f98bd1d53b2ea686
Alexander <piachev.a@ya.ru>
2023-03-14 03:36:09 +0800

    Заглушка menegement команды для загрузки БД из CSV

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Titles(models.Model):
+
+    pass
+
+
+class Categories(models.Model):
+
+    pass
+
+
+class Genres(models.Model):
+
+    pass
+
+
+class Reviews(models.Model):
+
+    pass
+
+
+class Comments(models.Model):
+
+    pass
