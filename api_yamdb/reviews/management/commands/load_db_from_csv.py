ac4d900d09ea15aa5a52b052cc1fe70bd20bd8f4
Alexander <piachev.a@ya.ru>
2023-03-14 12:57:38 +0800

    Interim change, create a basic database model and csv importer

--- api_yamdb/reviews/management/commands/load_db_from_csv.py
+++ api_yamdb/reviews/management/commands/load_db_from_csv.py
@@ -1,16 +1,40 @@
+import csv
+
 from django.core.management.base import BaseCommand, CommandError
-# from polls.models import Question as Poll
+from reviews.models import (
+    Category,
+    Comment,
+    Genre,
+    GenreTitle,
+    Review,
+    Title,
+    User
+)
 
 
 class Command(BaseCommand):
-    help = 'Загрузка тестовых данных из csv'
+    help = "Загрузка тестовых данных из csv"
 
     def add_arguments(self, parser):
-        parser.add_argument('path_to_csv', nargs='+', type=str)
+        parser.add_argument("path_to_csv", nargs="+", type=str)
 
     def handle(self, *args, **options):
-
         try:
-            print(options)
+            
+            self.load_categories()
         except Exception:
-            raise CommandError('Ошибка загрузки данных')
+            raise CommandError("Ошибка загрузки данных")
+
+    def load_categories(self):
+        with open('/run/media/greger/Disk_E/GRENOW/Dev/sprints/api_yamdb/api_yamdb/static/data/category.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            categories = []
+            for row in reader:
+                print(row)
+                categories.append(
+                    Category(
+                        name=row['name'],
+                        slug=row['slug']
+                    )
+                )
+            Category.objects.bulk_create(categories)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,29 +1,176 @@
+from datetime import datetime
+
 from django.contrib.auth import get_user_model
+from django.core.validators import (MaxValueValidator, MinValueValidator,
+                                    RegexValidator)
 from django.db import models
 
 User = get_user_model()
 
 
-class Titles(models.Model):
+class CategoriesGenresAbstract(models.Model):
+    """Абстрактная модель для жанров и категории"""
 
-    pass
+    name = models.CharField(
+        'Название',
+        max_length=256,
+    )
+
+    slug = models.SlugField(
+        'Slug',
+        max_length=50,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex='^[-a-zA-Z0-9_]+$'
+            )
+        ],
+    )
+
+    class Meta:
+        abstract = True
+        ordering = ('name', )
+
+    def __str__(self):
+        return self.name
 
 
-class Categories(models.Model):
+class Category(CategoriesGenresAbstract):
+    """Модель категории"""
 
-    pass
-
-
-class Genres(models.Model):
-
-    pass
-
-
-class Reviews(models.Model):
-
-    pass
+    class Meta(CategoriesGenresAbstract.Meta):
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        default_related_name = 'categories'
 
 
-class Comments(models.Model):
+class Genre(CategoriesGenresAbstract):
+    """Модель жанра"""
 
-    pass
+    class Meta(CategoriesGenresAbstract.Meta):
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+        default_related_name = 'genres'
+
+
+class Title(models.Model):
+    """Модель произведения"""
+
+    name = models.CharField(
+        'Название',
+        max_length=256,
+        db_index=True,
+    )
+    year = models.PositiveSmallIntegerField(
+        'Год выпуска',
+        db_index=True,
+        validators=(MaxValueValidator(int(datetime.now().year)),),
+    )
+    description = models.TextField(
+        'Описание',
+        db_index=True,
+        max_length=256,
+        blank=True,
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        through='GenreTitle',
+        related_name='titles',
+        verbose_name='Жанр',
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        related_name='titles',
+        verbose_name='Категория',
+    )
+
+    class Meta:
+        verbose_name = "Произведение"
+        verbose_name_plural = "Произведения"
+        ordering = ("id",)
+
+    def __str__(self):
+        return self.name
+
+
+class GenreTitle(models.Model):
+    """Вспомогательная модель многие к многим"""
+
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.genre} {self.title}'
+
+
+class Review(models.Model):
+    """Модель обзора и оценки произведения"""
+
+    text = models.CharField(
+        'Текст отзыва',
+        max_length=600
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации отзыва',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        verbose_name='Произведение'
+    )
+    score = models.IntegerField(
+        'Оценка',
+        default=1,
+        validators=(
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ),
+        error_messages={
+            'validators': 'Оценка от 1 до 10!'
+        }
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name='unique_autor'
+            )
+        ]
+
+    def __str__(self):
+        return f'Обзор {self.author} на {self.title}'
+
+
+class Comment(models.Model):
+    """Модель комментария к произведению"""
+
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    pub_date = models.DateTimeField(
+        'Дата комментария',
+        auto_now_add=True,
+        db_index=True
+    )
+    text = models.TextField()
+
+    def __str__(self):
+        return self.author
