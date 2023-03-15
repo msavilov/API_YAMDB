351a569ba8ef1426d0c4307c56e0c92bbfc42f56
Юлия Мелик-Гусейнова <yu.melik-guseinova@yandex.ru>
2023-03-16 00:36:22 +0300

    api

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,18 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+                    ReviewViewSet, TitlesViewSet, )
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'genres', GenreViewSet, basename='genre')
+router.register(r'titles', TitlesViewSet, basename='titles')
+router.register(r'titles/(?P<title_id>\d+)/reviews',
+                ReviewViewSet, basename='reviews')
+router.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)'
+                r'/comments', CommentViewSet, basename='comments')
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+]
