2b6407b2d6e0c42df0422ad0513a9acec1890316
Maksim Savilov <m.savilov@yandex.ru>
2023-03-24 19:36:48 +0300

    correct remarks from codereview_3

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -6,26 +6,15 @@ from reviews.models import Title
 class TitlesFilter(FilterSet):
     genre = CharFilter(
         field_name='genre__slug',
-        method='filter_by_genre',
+        lookup_expr='icontains',
     )
     category = CharFilter(
         field_name='category__slug',
-        method='filter_by_category',
     )
     name = CharFilter(
         field_name='name',
-        method='filter_by_name',
     )
 
     class Meta:
         model = Title
         fields = ('name', 'category', 'genre', 'year')
-
-    def filter_by_genre(self, queryset, name, value):
-        return queryset.filter(genre__slug=value)
-
-    def filter_by_category(self, queryset, name, value):
-        return queryset.filter(category__slug=value)
-
-    def filter_by_name(self, queryset, name, value):
-        return queryset.filter(name=value)
--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 from .models import User
+from reviews.models import Review
 
-admin.site.register(User)
+admin.site.register(Review, User)
