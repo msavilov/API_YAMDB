1a2277ed44a6a28fae9c43a97039cbccb63342d3
Maksim Savilov <m.savilov@yandex.ru>
2023-03-24 20:12:02 +0300

    fix mistake

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 from .models import User
 from reviews.models import Review
 
-admin.site.register(Review, User)
+admin.site.register(User)
+admin.site.register(Review)
